03de45158dfe50b51047822150e697fc54b9ffc2
Salam <salam73@gmail.com>
2019-07-01 14:23:08 +0200

    .changing

--- ProjectFolder/App.js
+++ ProjectFolder/App.js
@@ -23,7 +23,7 @@ export default class App extends React.Component {
         <View style={styles.container}>
           <Image source={require('./assets/ReactNativeFirebase.png')} style={[styles.logo]}/>
           <Text style={styles.welcome}>
-            Welcome to {'\n'} Hello world
+            Welcome to {'\n'} مرحبة سلام
           </Text>
           <Text style={styles.instructions}>
             To get started, edit App.js
